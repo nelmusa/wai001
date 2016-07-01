a09b774d5d68e8d5ebcca41868f82e23e31494e7
Raul Luna <rluna@go-labs.net>
2016-07-01 10:01:05 -0600

    implementing ios notifications

--- js/index.js
+++ js/index.js
@@ -37,11 +37,54 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+        try {
+          var idnotify = localStorage.idnotify;
+          if (idnotify == null || idnotify == "" || idnotify == undefined){
+              var pushNotification = window.plugins.pushNotification;
+              pushNotification.register(this.successHandler, this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"});
+          }
+        }catch(err) {
+        }
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
         console.log('Received Event: ' + id);
+    },
+    errorHandler: function(e) {
+    },
+    successHandler: function(result) {
+        try {
+            localStorage.idnotify = result;
+            var notification = localStorage.notify;
+            if (notification == null || notification == "" || notification == 'si' || notification == undefined){
+              var request = new XMLHttpRequest();
+              request.open("GET", "http://www.wai-news.com/index.php?option=com_jbackend&view=request&action=put&module=push&resource=register&token=" + result + "&appcode=nms.wai.001&platform=ios&ios_alert=1&ios_badge=1&ios_sound=1",
+              true);
+              request.send();
+            }
+        }catch(err) {
+        }
+    },
+    onNotificationAPN: function(event) {
+        var pushNotification = window.plugins.pushNotification;
+        try {
+            alert(event.body);
+            event.body = JSON.parse(event.body);
+            if (event.body.data.badge){
+                pushNotification.setApplicationIconBadgeNumber(this.errorHandler, this.errorHandler, event.body.data.badge);
+            }
+            if (event.body.data.sound) {
+                var snd = new Media(event.body.data.sound);
+                snd.play();
+            }else {
+                var snd = new Media(event.sound);
+                snd.play();
+            }
+            alert('push APN full event ' + JSON.stringify(event));
+        }catch(err) {
+            alert(err);
+        }
     }
 };
